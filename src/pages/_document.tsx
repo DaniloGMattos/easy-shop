fa4da43eb8fb6e7cc27346cf10e997fc6c26b530
Danilo Mattos <mattosgdanilo@gmail.com>
2022-11-27 22:33:25 -0300

    feat: initial stitches configuration

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { getCssText } from "../styles";
 
 export default function Document() {
   return (
@@ -24,6 +25,10 @@ export default function Document() {
           href="https://fonts.googleapis.com/css?family=Roboto:100,100italic,300,300italic,regular,italic,500,500italic,700,700italic,900,900italic&display=swap"
           rel="stylesheet"
         />
+        <style
+          id="stitches"
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        ></style>
       </Head>
       <body>
         <Main />
---
+++ src/styles/index.ts
@@ -0,0 +1,17 @@
+import { createStitches } from "@stitches/react";
+
+export const {
+  config,
+  styled,
+  css,
+  getCssText,
+  globalCss,
+  theme,
+  createTheme,
+} = createStitches({
+  theme: {
+    colors: {
+      primary: "#ff0000",
+    },
+  },
+});
